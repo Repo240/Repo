6188d86cc45332ea6e8e2b02390fb992439c1a0b
DaniHP <DaniHP@>
2024-10-03 17:23:18 +0200

    1

---
+++ src/it/dv/main/Start.java
@@ -0,0 +1,12 @@
+package it.dv.main;
+
+import java.util.Date;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Start: "+ new Date());
+	}
+
+}
