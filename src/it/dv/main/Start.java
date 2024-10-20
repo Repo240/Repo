cdd69d81d67bfe0181542abfd6a0bbbf175ca50a
DaniHP <DaniHP@>
2024-10-20 21:22:56 +0200

    modifica 1branch

--- src/it/dv/main/Start.java
+++ src/it/dv/main/Start.java
@@ -6,7 +6,7 @@ public class Start {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Start : "+ new Date());
+		System.out.println("Start 2: "+ new Date());
 	}
 
 }
